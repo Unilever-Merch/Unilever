a995f9086c7096a287ad6084dbd0b8c89dd4245c
Yasmim Guerra Amorim <rafinhaffiel@gmail.com>
2026-01-27 13:09:25 -0300

    Atualização de VPL

--- js/consulta.js
+++ js/consulta.js
@@ -25,7 +25,7 @@ function buscarRE() {
     return;
   }
 
-  const meses = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV'];
+  const meses = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'];
 
   let html = `<h2 style="text-align:center; margin-bottom: 1rem;">RE: ${re}</h2>`;
   html += `<div class="table-container"><table>`;
